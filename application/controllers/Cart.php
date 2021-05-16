30f3378611492db192f37ff488d84781c4ff7b92
Lucky7Tb <Luckytri101@gmail.com>
2021-05-16 09:17:27 +0700

    add checkig user is login or not

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -3,6 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cart extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		
+		if (isNotLogin()) {
+			redirect('auth');
+		}
+	}
+
 	public function index()
 	{
 		$data['title'] = 'Cart';
--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -3,6 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Profile extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		
+		if (isNotLogin()) {
+			redirect('auth');
+		}
+	}
+
 	public function index()
 	{
 		$data['title'] = 'Profile';
